8349f33b8221272fab8a9050cf9e013e0882a4ab
ChiSaiM <944125621@qq.com>
2018-10-10 14:05:12 +0800

    config-server and config-client and config-eureka-server

--- config-eureka-server/src/main/java/home/pb/springcloud/configeurekaserver/ConfigEurekaServerApplication.java
+++ config-eureka-server/src/main/java/home/pb/springcloud/configeurekaserver/ConfigEurekaServerApplication.java
@@ -1,20 +1,17 @@
-package home.pb.springcloud.configserver;
+package home.pb.springcloud.configeurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author ChiSaiM
  */
 @SpringBootApplication
-@EnableConfigServer
 @EnableEurekaServer
-public class ConfigServerApplication {
+public class ConfigEurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class, args);
+        SpringApplication.run(ConfigEurekaServerApplication.class, args);
     }
 }
